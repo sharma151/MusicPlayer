2f891777a6d1a63cc163d227ba8178001f497c06
sharma151 <sharmasaurav150503@gmail.com>
2024-09-02 15:51:28 +0545

    activity section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
       colors: {
         grey: "#514A4A",
+        orange: "#E09E3D",
       },
       width: {
         "199px": "199px",
@@ -27,18 +28,28 @@ module.exports = {
       height: {
         "982px": "982px",
         "413px": "413px",
+        287: "287",
         "248px": "248px",
         "197px": "197px",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+
       boxShadow: {
         "custom-shadow": "0px 4px 20px 5px rgba(0, 0, 0, 0.25)", // Equivalent to #00000040
         custom: "0px 11px 15px 0px #00000040",
+
+        "custom-orange": "0px 2px 20px 0px #E09E3D",
+        "friends-logo": "0px 4px 15px 2px #00000040",
+        "box-shadow": "0px 4px 20px 5px #00000040",
       },
       backgroundImage: {
         "gradient-custom": "linear-gradient(to right, #f3a847, #f6d365)", // Custom gradient
+        "custom-gradient-junoon":
+          "linear-gradient(210.57deg, #F62545 1.42%, #590819 96.55%)",
+        "button-background":
+          "linear-gradient(180deg, #F8921A 0%, #F62545 100%)",
       },
     },
   },
